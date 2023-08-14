cbe65935c46ce80a8a3342e046b9e97b03a84bff
jamaludheen kp <jamaljazz1998@gmail.com>
2023-08-14 10:47:02 +0530

    why is trueway section heading and paragraph creat

---
+++ src/Components/WhyisTrueway.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+function WhyisTrueway() {
+  return (
+    <div>
+        <div className='w-full h-full flex flex-col justify-between'>
+            <div className="grid max-w-[1240px] m-auto my-20">
+                <div className='flex flex-col justify-center md:items-center w-full px-2 py-4 md:pl-16'>
+                    <h2 className='text-3xl md:text-4xl tracking-wide font-bold text-center'> Why Is <b className='text-green-500'>Trueway</b> Better In Attestation Services? </h2>
+                    <p className='my-5 text-center px-10 md:px-40 font-thin text-gray-700'>
+                    <b >Trueway International </b> is the best of the best in embassy attestation services for migrating people. What makes us No.1 from other certificate attestation consultancies is the fact that there is no inference or third party between our customers and us. Our services are trustworthy, fast, quick, and hassle-free.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default WhyisTrueway;
