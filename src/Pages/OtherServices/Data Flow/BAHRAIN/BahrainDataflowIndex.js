aaaab60aa92936414e8213f35161fb0888bb4890
jamaludheen kp <jamaljazz1998@gmail.com>
2023-10-17 10:42:22 +0530

    Process for Bahrain Dataflow

--- src/Pages/OtherServices/Data Flow/BAHRAIN/BahrainDataflowIndex.js
+++ src/Pages/OtherServices/Data Flow/BAHRAIN/BahrainDataflowIndex.js
@@ -2,6 +2,7 @@ import React from 'react'
 import BahrainDFhero from './BahrainDFhero'
 import ContactforDataflow from '../ContactforDataflow'
 import PSVinBahrain from './PSVinBahrain'
+import ProcessForBahrain from './ProcessForBahrain'
 
 function BahrainDataflowIndex() {
   return (
@@ -9,6 +10,7 @@ function BahrainDataflowIndex() {
         <BahrainDFhero/>
         <ContactforDataflow/>
         <PSVinBahrain/>
+        <ProcessForBahrain/>
     </div>
   )
 }
