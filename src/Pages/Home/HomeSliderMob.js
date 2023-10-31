1c871966cd5117ae218653ce858fa42bcaa4435d
jamaludheen kp <jamaljazz1998@gmail.com>
2023-10-31 14:51:02 +0530

    update mobile slider home page

--- src/Pages/Home/HomeSliderMob.js
+++ src/Pages/Home/HomeSliderMob.js
@@ -34,7 +34,7 @@ function HomeSliderMob() {
         clickable:true
         }}
         autoplay={{
-          delay: 2500,
+          delay: 3000,
           disableOnInteraction: false,
         }}
         centeredSlides={true}
@@ -43,7 +43,7 @@ function HomeSliderMob() {
         <SwiperSlide className='md:flex items-center object-center'>
         <div className="image relative">
           <img src={slider1} alt="slider_1" className='object-center w-auto'/>
-          <div className=" absolute top-[8px] px-5 ">
+          <div className=" absolute top-[5px] pl-5 ">
             <h3 className='text-[30px] md:text-[48px] font-semibold leading-10 md:leading-normal py-2 md:px-5 font-HeadingFont'> Hello From <br /><b className='md:text-[60px] tracking-normal'>Trueway International</b></h3>
             <p className='font-poppins md:text-[30px]  text-[14px] w-[100%] md:px-8 justify-center text-gray-600 '>
             Trueway International is an ISO-assured confirmation organisation providing UAE Attestation Services in India and is affirmed by Ministries and government offices in India and abroad. Main services include Attestation, Dataflow Verification, Good Standing and Council Verifications, College & Hospital Verifications, PCC, BLS & ACLS, Coaching.
@@ -57,8 +57,8 @@ function HomeSliderMob() {
         <SwiperSlide className='flex items-center'>
         <div className="image relative">
           <img src={slider2} alt="slider_2" className='object-center w-auto'/>
-          <div className=" absolute top-[10px] px-5 ">
-            <h3 className='text-[32px] md:text-[48px] font-semibold leading-10 md:leading-normal py-2 md:px-5 font-HeadingFont'> Make The Migration<br /><b className='md:text-[60px] tracking-normal'>Smooth With Dataflow</b></h3>
+          <div className=" absolute top-[5px] pl-5 ">
+            <h3 className='text-[30px] md:text-[48px] font-semibold leading-10 md:leading-normal py-2 md:px-5 font-HeadingFont'> Make The Migration<br /><b className='md:text-[60px] tracking-normal'>Smooth With Dataflow</b></h3>
             <p className='font-poppins md:text-[30px]  text-[14px] w-[100%] md:px-8 justify-center text-gray-600 '>
             For every migration, data flow is significant. The types of dataflow services we provide are DHA, HAAD, MOH, Saudi Arabia, Qatar, Bahrain, Kuwait, Oman. In order to prove the authenticity of the documents you submitted, Dataflow is mandatory.
             </p>
@@ -71,9 +71,9 @@ function HomeSliderMob() {
         <SwiperSlide className='flex items-center'>
         <div className="image relative">
           <img src={slider3} alt="slider_1" className='object-center w-auto'/>
-          <div className=" absolute top-[10px] px-5 font-[poppins]">
-            <h3 className='text-[32px] md:text-[54px] font-bold leading-10 md:leading-normal py-3 md:px-5 font-HeadingFont'>The Complete Option for Exam Coaching</h3>
-            <p className='font-poppins md:text-[32px]  text-[14px] w-[100%] md:px-8 justify-center text-gray-600 '>
+          <div className=" absolute top-[5px] pl-5 ">
+            <h3 className='text-[30px] md:text-[54px] font-bold leading-10 md:leading-normal md:px-5 font-HeadingFont'>The Complete Option for Exam Coaching</h3>
+            <p className='font-poppins md:text-[32px]  text-[14px] w-[100%] md:px-8 justify-center text-gray-600 py-1'>
             Trying to achieve the highest scores in the exams to go abroad? For that, you need a complete academic preparation course. We provide coaching for IELTS, OET, and Prometric exams. We teach you the key skills and give you a better understanding of the subject.
             </p>
             <button className='flex rounded-lg p-2 my-2 px-4  bg-transparent border-solid border-2 border-green-600 md:text-3xl md:p-3 md:m-5 md:my-5'>
@@ -86,12 +86,12 @@ function HomeSliderMob() {
         <div className="image relative">
           <img src={slider4} alt="slider_4" className='object-center w-auto'/>
           <div className=" absolute top-[10px] px-5 font-[poppins]">
-            <h3 className='text-[32px] md:text-[58px] font-bold leading-10 md:leading-normal py-3 md:px-5 font-HeadingFont'> Is Employment Abroad your dream? </h3>
+            <h3 className='text-[30px] md:text-[54px] font-bold leading-10 md:leading-normal py-3 md:px-5 font-HeadingFont'> Is Employment Abroad your dream? </h3>
             <p className='font-poppins md:text-[32px]  text-[14px] w-[100%] md:px-8 justify-center text-gray-600 '>
             Verification, Attestation, data flow, and many more are needed to get visa approval to fly abroad. All this will make you mad, but with Trueway International, all of these are easy, quick, and trustworthy.
             </p>
             <button className='flex rounded-lg p-2 my-2 px-4  bg-transparent border-solid border-2 border-green-600 md:text-3xl md:p-3 md:m-5 md:my-5'>
-             <BiSolidPhoneCall className='text-green-700 text-xl mx-2  md:text-3xl '/> <a href="tel:+918327626262">Connect with our customer care </a>
+             <BiSolidPhoneCall className='text-green-700 text-xl mx-2  md:text-3xl '/> <a href="tel:+918327626262">Free Consulting Now</a>
             </button>
           </div>
         </div>
