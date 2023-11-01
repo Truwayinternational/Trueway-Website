48f0c68d482f634398173775f8c513cc52cc76ae
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-01 18:26:51 +0530

    qatar embassy - testimonial , our commitment, faq section all added

--- src/Pages/OtherServices/Embassy Attestation/Qatar/QatarEmbassyIndex.js
+++ src/Pages/OtherServices/Embassy Attestation/Qatar/QatarEmbassyIndex.js
@@ -3,6 +3,10 @@ import QatarEmbassyHero from './QatarEmbassyHero'
 import SiginificanceOfQatarEmbassy from './SiginificanceOfQatarEmbassy'
 import HowDoIGetQatar from './HowDoIGetQatar'
 import DocumentsRequired from './DocumentsRequired'
+import Form2 from '../../../../Components/Forms/Form2'
+import OurCommitment from '../../../../Components/OurCommitment'
+import OurClients from '../../OurClients'
+import FaqQatar from './FaqQatar'
 
 
 
@@ -13,6 +17,10 @@ function qatarEmbassyIndex() {
         <SiginificanceOfQatarEmbassy/>
         <HowDoIGetQatar/>
         <DocumentsRequired/>
+        <Form2/>
+        <OurCommitment/>
+        <OurClients/>
+        <FaqQatar/>
     </div>
   )
 }
