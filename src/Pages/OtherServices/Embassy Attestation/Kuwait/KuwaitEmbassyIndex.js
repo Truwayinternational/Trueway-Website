3439becaee5770b609489dc66c1ba48b670bac19
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-02 12:28:45 +0530

    Purposes of kuwait embassy attestation

--- src/Pages/OtherServices/Embassy Attestation/Kuwait/KuwaitEmbassyIndex.js
+++ src/Pages/OtherServices/Embassy Attestation/Kuwait/KuwaitEmbassyIndex.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import KuwaitEmbassyHero from './KuwaitEmbassyHero'
+import PurposeOfKuwaitEmbassy from './PurposeOfKuwaitEmbassy'
 
 
 function KuwaitEmbassyIndex() {
   return (
     <div>
         <KuwaitEmbassyHero/>
+        <PurposeOfKuwaitEmbassy/>
     </div>
   )
 }
