81171ff183d578e86cc64364481da016a258a048
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-02 17:43:26 +0530

    Purpose of uae embassy attestation - page

--- src/Pages/OtherServices/Embassy Attestation/Uae/UaeEmbassyIndex.js
+++ src/Pages/OtherServices/Embassy Attestation/Uae/UaeEmbassyIndex.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import UaeEmbassyHero from './UaeEmbassyHero'
+import PurposeOfUaeEmbassy from './PurposeOfUaeEmbassy'
+
 
 function UaeEmbassyIndex() {
   return (
     <div>
         <UaeEmbassyHero/>
+        <PurposeOfUaeEmbassy/>
     </div>
   )
 }
