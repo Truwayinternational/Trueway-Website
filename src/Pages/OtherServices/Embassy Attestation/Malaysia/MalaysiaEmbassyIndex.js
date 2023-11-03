138b3f9760bd8a7ccad7d35ddb42419913041ecc
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-03 15:58:27 +0530

    PURPOSE OF MALAYSIA EMBASSY

--- src/Pages/OtherServices/Embassy Attestation/Malaysia/MalaysiaEmbassyIndex.js
+++ src/Pages/OtherServices/Embassy Attestation/Malaysia/MalaysiaEmbassyIndex.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import MalaysiaEmbassyHero from './MalaysiaEmbassyHero'
+import PurposeOfMalaysiaEmbassy from './PurposeOfMalaysiaEmbassy'
 
 function MalaysiaEmbassyIndex() {
   return (
     <div>
         <MalaysiaEmbassyHero/>
+        <PurposeOfMalaysiaEmbassy/>
     </div>
   )
 }
