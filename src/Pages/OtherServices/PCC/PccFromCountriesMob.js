243f7dd56a6b83ef0d35ca1c2bd1b56c9ed951d1
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-04 14:40:38 +0530

    Get PCC from GCC countries swiftly -- SECTION FROM PCC HOME

---
+++ src/Pages/OtherServices/PCC/PccFromCountriesMob.js
@@ -0,0 +1,108 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+
+import { Autoplay} from 'swiper/modules';
+
+// added countries logs 
+import PccUae from '../../../Assets/pcc/PCC UAE.png'
+import PccSaudi from '../../../Assets/pcc/PCC Saudi.png'
+import PccQatar from '../../../Assets/pcc/PCC Qatar.png'
+import PccOman from '../../../Assets/pcc/PCC Oman.png'
+import PccKuwait from '../../../Assets/pcc/PCC Kuwait.png'
+
+
+
+function PccFromCountriesMob() {
+  return (
+    <div className="lg:hidden flex mx-auto px-10  max-w-[1240px]">
+    <Swiper 
+        className="w-full h-full "
+        slidesPerView={1}
+        spaceBetween={10}
+        autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+        }}
+        breakpoints={{
+        '250': {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        '320': {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        '480': {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        '1020': {
+          slidesPerView: 3,
+          spaceBetween: 10,
+        }
+        }}
+        modules={[ Autoplay]}>
+
+        <SwiperSlide>
+            <div className='flex flex-col w-full  bg-white border border-zinc-400 rounded-3xl hover:bg-green-500 hover:text-white cursor-pointer p-4'>
+                <img className='px-3 w-[150px] mx-auto' src={PccUae} alt="PCC UAE" />
+                <a href="/pcc_uae">
+                    <h1 className='font-HeadingFont md:text-lg text-md font-bold text-center pb-4 '>
+                    PCC UAE
+                    </h1>
+                </a>
+            </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+            <div className='flex flex-col w-full  bg-white border border-zinc-400 rounded-3xl hover:bg-green-500 hover:text-white cursor-pointer p-4'>
+                <img className='px-3 w-[150px] mx-auto' src={PccSaudi} alt="PCC SAUDI" />
+                <a href="/pcc_ksa">
+                    <h1 className='font-HeadingFont md:text-lg text-md font-bold text-center pb-4 '>
+                        PCC SAUDI
+                    </h1>
+                </a>
+            </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+            <div className='flex flex-col w-full  bg-white border border-zinc-400 rounded-3xl hover:bg-green-500 hover:text-white cursor-pointer p-4'>
+                <img className='px-3 w-[150px] mx-auto' src={PccQatar} alt="PCC QATAR" />
+                <a href="/pcc_qatar">
+                    <h1 className='font-HeadingFont md:text-lg text-md font-bold text-center pb-4 '>
+                    PCC QATAR 
+                    </h1>
+                </a>
+            </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+            <div className='flex flex-col w-full  bg-white border border-zinc-400 rounded-3xl hover:bg-green-500 hover:text-white cursor-pointer p-4'>
+                <img className='px-3 w-[150px] mx-auto' src={PccOman} alt="PCC OMAN" />
+                <a href="/pcc_oman">
+                    <h1 className='font-HeadingFont md:text-lg text-md font-bold text-center pb-4 '>
+                        PCC OMAN 
+                    </h1>
+                </a>
+            </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+        <div className='flex flex-col  bg-white border border-zinc-400 rounded-3xl hover:bg-green-500 hover:text-white cursor-pointer p-4'>
+            <img className='px-3 w-[150px] mx-auto' src={PccKuwait} alt="PCC KUWAIT" />
+             <a href="/pcc_kuwait">
+                <h1 className='font-HeadingFont md:text-lg text-md font-bold text-center pb-4'>
+                    PCC KUWAIT
+                </h1>
+             </a>
+        </div>
+        </SwiperSlide>
+
+    </Swiper>
+    </div>
+  )
+}
+
+export default PccFromCountriesMob
--- src/Pages/OtherServices/PCC/PccIndex.js
+++ src/Pages/OtherServices/PCC/PccIndex.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import PccHeroPage from './PccHeroPage'
+import PccFromCountries from './PccFromCountries'
 
 
 function PccIndex() {
   return (
     <div>
         <PccHeroPage/>
+        <PccFromCountries/>
     </div>
   )
 }
