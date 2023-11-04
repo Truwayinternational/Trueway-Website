ba2e0b54f23f1fad32579eaf070fb673b1fc8941
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-04 13:57:13 +0530

    PCC - page created , hero and index.js created

---
+++ src/Pages/OtherServices/PCC/PccIndex.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PccHeroPage from './PccHeroPage'
+
+
+function PccIndex() {
+  return (
+    <div>
+        <PccHeroPage/>
+    </div>
+  )
+}
+
+export default PccIndex
