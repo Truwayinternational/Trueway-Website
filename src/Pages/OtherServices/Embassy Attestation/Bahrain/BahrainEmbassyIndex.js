8914ca54925bee0bebf7e575fd9487664087809a
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-04 11:02:19 +0530

    Purpose of Bahrain embassy

--- src/Pages/OtherServices/Embassy Attestation/Bahrain/BahrainEmbassyIndex.js
+++ src/Pages/OtherServices/Embassy Attestation/Bahrain/BahrainEmbassyIndex.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import BahrainEmbassyHero from './BahrainEmbassyHero'
+import PurposeOfBahrainEmbassy from './PurposeOfBahrainEmbassy'
 
 
 function BahrainEmbassyIndex() {
   return (
     <>
         <BahrainEmbassyHero/>
+        <PurposeOfBahrainEmbassy/>
     </>
   )
 }
