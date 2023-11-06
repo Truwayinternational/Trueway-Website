f114bf3c242841f8b8f01cbdee30880e5e54d954
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-06 16:09:32 +0530

    purpose of qatar pcc -

--- src/Pages/OtherServices/PCC/QATAR/QatarPccIndex.js
+++ src/Pages/OtherServices/PCC/QATAR/QatarPccIndex.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import QatarPccHero from './QatarPccHero'
+import PurposeOfQatarPcc from './PurposeOfQatarPcc'
 
 
 function QatarPccIndex() {
   return (
     <div>
         <QatarPccHero/>
+        <PurposeOfQatarPcc/>
     </div>
   )
 }
