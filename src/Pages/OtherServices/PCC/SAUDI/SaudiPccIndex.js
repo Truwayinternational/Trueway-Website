b90b8985148a8007539856f8f4737d59bdc0bb9a
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-06 15:40:15 +0530

    SAUDI PCC - completed all setctions- add testiomonial, our commit

--- src/Pages/OtherServices/PCC/SAUDI/SaudiPccIndex.js
+++ src/Pages/OtherServices/PCC/SAUDI/SaudiPccIndex.js
@@ -2,6 +2,10 @@ import React from 'react'
 import SaudiPccHero from './SaudiPccHero'
 import PorposeOfSaudiPcc from './PorposeOfSaudiPcc'
 import HowToApplySaudiPcc from './HowToApplySaudiPcc'
+import OurCommitment from '../../../../Components/OurCommitment'
+import OurClients from '../../OurClients'
+import Form2 from '../../../../Components/Forms/Form2'
+import FaqSaudiPcc from './FaqSaudiPcc'
 
 
 function SaudiPccIndex() {
@@ -10,6 +14,10 @@ function SaudiPccIndex() {
         <SaudiPccHero/>
         <PorposeOfSaudiPcc/>
         <HowToApplySaudiPcc/>
+        <OurCommitment/>
+        <OurClients/>
+        <Form2/>
+        <FaqSaudiPcc/>
     </div>
   )
 }
