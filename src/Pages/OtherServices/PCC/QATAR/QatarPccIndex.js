ec864ca69d3905d4dc7d7a7878b4e6e0b7e124ab
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-06 16:25:45 +0530

    Qatar Pcc - completed  added- testimonial, our commitment faq qatar

--- src/Pages/OtherServices/PCC/QATAR/QatarPccIndex.js
+++ src/Pages/OtherServices/PCC/QATAR/QatarPccIndex.js
@@ -2,6 +2,10 @@ import React from 'react'
 import QatarPccHero from './QatarPccHero'
 import PurposeOfQatarPcc from './PurposeOfQatarPcc'
 import HowToApplyQatarPcc from './HowToApplyQatarPcc'
+import OurCommitment from '../../../../Components/OurCommitment'
+import OurClients from '../../OurClients'
+import Form2 from '../../../../Components/Forms/Form2'
+import FaqQatarPcc from './FaqQatarPcc'
 
 
 function QatarPccIndex() {
@@ -10,6 +14,10 @@ function QatarPccIndex() {
         <QatarPccHero/>
         <PurposeOfQatarPcc/>
         <HowToApplyQatarPcc/>
+        <OurCommitment/>
+        <OurClients/>
+        <Form2/>
+        <FaqQatarPcc/>
     </div>
   )
 }
