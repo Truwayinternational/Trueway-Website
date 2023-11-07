538f686f5abe57a1cb0066542c4aaacd7cb5c60e
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-07 11:04:28 +0530

    How to apply Oman pcc

--- src/Pages/OtherServices/PCC/OMAN/OmanPccIndex.js
+++ src/Pages/OtherServices/PCC/OMAN/OmanPccIndex.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import OmanPccHero from './OmanPccHero'
 import WhyNeedOmanPcc from './WhyNeedOmanPcc'
+import HowToApplyOmanPcc from './HowToApplyOmanPcc'
 
 function OmanPccIndex() {
   return (
     <div>
         <OmanPccHero/>
         <WhyNeedOmanPcc/>
+        <HowToApplyOmanPcc/>
     </div>
   )
 }
