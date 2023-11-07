43a0b23ccb92a4bef33142274510c34746c5eccb
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-07 13:17:00 +0530

    How to apply kuwait pcc

--- src/Pages/OtherServices/PCC/KUWAIT/KuwaitPccIndex.js
+++ src/Pages/OtherServices/PCC/KUWAIT/KuwaitPccIndex.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import KuwaitPccHero from './KuwaitPccHero'
 import WhyIsPccRequired from './WhyIsPccRequired'
+import HowToApplyKuwaitPcc from './HowToApplyKuwaitPcc'
 
 function KuwaitPccIndex() {
   return (
     <>
         <KuwaitPccHero/>
         <WhyIsPccRequired/>
+        <HowToApplyKuwaitPcc/>
     </>
   )
 }
