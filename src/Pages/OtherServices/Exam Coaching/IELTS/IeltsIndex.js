e016a71cbcbdc08adb5b0cb7a2b3e2e6bdad03ed
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-10 11:40:33 +0530

    who and why IELTS web cards and mobile slider

--- src/Pages/OtherServices/Exam Coaching/IELTS/IeltsIndex.js
+++ src/Pages/OtherServices/Exam Coaching/IELTS/IeltsIndex.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import IeltsHero from './IeltsHero'
 import WhatIsIELTS from './WhatIsIELTS'
+import WhoandWhyIelts from './WhoandWhyIelts'
 
 function IeltsIndex() {
   return (
     <div>
         <IeltsHero/>
         <WhatIsIELTS/>
+        <WhoandWhyIelts/>
     </div>
   )
 }
