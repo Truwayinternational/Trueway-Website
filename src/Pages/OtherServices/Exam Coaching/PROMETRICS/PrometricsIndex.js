5a34583a26b3a820c4899aadd269915ee57476e7
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-10 15:32:00 +0530

    What is prometric

--- src/Pages/OtherServices/Exam Coaching/PROMETRICS/PrometricsIndex.js
+++ src/Pages/OtherServices/Exam Coaching/PROMETRICS/PrometricsIndex.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import PrometricsHero from './PrometricsHero'
+import WhatisPrometric from './WhatisPrometric'
 
 
 function PrometricsIndex() {
   return (
     <div>
         <PrometricsHero/>
+        <WhatisPrometric/>
     </div>
   )
 }
