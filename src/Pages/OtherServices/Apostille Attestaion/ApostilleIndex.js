f6d12a86fbe9d47de82515466f7269059841ff9f
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-13 11:42:49 +0530

    Whats is Apostille Attestation

--- src/Pages/OtherServices/Apostille Attestaion/ApostilleIndex.js
+++ src/Pages/OtherServices/Apostille Attestaion/ApostilleIndex.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ApostilleHero from './ApostilleHero'
+import WhatIsApostille from './WhatIsApostille'
 
 function ApostilleIndex() {
   return (
     <div>
         <ApostilleHero/>
+        <WhatIsApostille/>
     </div>
   )
 }
