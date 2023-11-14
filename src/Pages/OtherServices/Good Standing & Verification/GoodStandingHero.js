244dda1bac4180f2f463de0534ad2fa56ccf9831
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-14 12:08:37 +0530

    Siginificance of good standing

--- src/Pages/OtherServices/Good Standing & Verification/GoodStandingHero.js
+++ src/Pages/OtherServices/Good Standing & Verification/GoodStandingHero.js
@@ -17,7 +17,6 @@ function GoodStandingHero() {
               <img className='object-cover ' src={goodstanding_banner} alt="page_banner" />
             </div>
 
-            {/* responsive banner of MOFA attestation */}
             <div className='w-full top-0 lg:ml-20 relative lg:hidden'>
               <img className='object-contain w-full ' src={banner_mob} alt="page_banner" />
             </div>
--- src/Pages/OtherServices/Good Standing & Verification/GoodStandingIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/GoodStandingIndex.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import GoodStandingHero from './GoodStandingHero'
+import SiginificanceOfGoodstandig from './SiginificanceOfGoodstandig'
 
 function GoodStandingIndex() {
   return (
     <div>
         <GoodStandingHero/>
+        <SiginificanceOfGoodstandig/>
     </div>
   )
 }
