72a83341d67af81c4efa57e996a282b45dbf475c
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-16 12:49:08 +0530

    We Provide Good Standing for these Countries

--- src/Pages/OtherServices/Good Standing & Verification/Kerala Nursing/KeralaNursingIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Kerala Nursing/KeralaNursingIndex.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import KeralaNursingHero from './KeralaNursingHero'
+import WeProvideGoodstanding from './WeProvideGoodstanding'
 
 function KeralaNursingIndex() {
   return (
+
     <div>
         <KeralaNursingHero/>
+        <WeProvideGoodstanding/>  
     </div>
   )
 }
