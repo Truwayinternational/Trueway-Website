a0da263b6ff40c51cde1b19030887611a474d3c4
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-16 15:24:24 +0530

    tamil nadu nursing council - completed - add testiimonial, faq sections

--- src/Pages/OtherServices/Good Standing & Verification/TamilNadu Nursing/TamilNaduNursingIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/TamilNadu Nursing/TamilNaduNursingIndex.js
@@ -2,6 +2,10 @@ import React from 'react'
 import TamilNaduNursingHero from './TamilNaduNursingHero'
 import WeProvideTamilnadu from './WeProvideTamilnadu'
 import DocumentsRequiredTamilNadu from './DocumentsRequiredTamilNadu'
+import WhyShouldYouChooseUs from '../../../../Components/WhyShouldYouChooseUs'
+import OurClients from '../../OurClients'
+import Form2 from '../../../../Components/Forms/Form2'
+import FaqTamilNaduCouncil from './FaqTamilNaduCouncil'
 
 function TamilNaduNursingIndex() {
   return (
@@ -9,6 +13,10 @@ function TamilNaduNursingIndex() {
         <TamilNaduNursingHero/>
         <WeProvideTamilnadu/>
         <DocumentsRequiredTamilNadu/>
+        <WhyShouldYouChooseUs/>
+        <OurClients/>
+        <Form2/>
+        <FaqTamilNaduCouncil/>
     </div>
   )
 }
