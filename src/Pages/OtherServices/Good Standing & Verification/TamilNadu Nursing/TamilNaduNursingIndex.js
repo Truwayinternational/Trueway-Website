2336d93e6439ff21fda44c8d1506eb0e2065a35e
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-16 15:18:38 +0530

    Document Required for tamil nadu nursing council

--- src/Pages/OtherServices/Good Standing & Verification/TamilNadu Nursing/TamilNaduNursingIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/TamilNadu Nursing/TamilNaduNursingIndex.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import TamilNaduNursingHero from './TamilNaduNursingHero'
 import WeProvideTamilnadu from './WeProvideTamilnadu'
+import DocumentsRequiredTamilNadu from './DocumentsRequiredTamilNadu'
 
 function TamilNaduNursingIndex() {
   return (
     <div>
         <TamilNaduNursingHero/>
         <WeProvideTamilnadu/>
+        <DocumentsRequiredTamilNadu/>
     </div>
   )
 }
