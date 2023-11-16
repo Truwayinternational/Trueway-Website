4c3b8048b4599774efc9434e04fadbaa740e1d80
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-16 15:44:57 +0530

    We Provide Good Standing for these Countries - karnataka council

--- src/Pages/OtherServices/Good Standing & Verification/Karnataka Nursing/KarnatakaNursingIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Karnataka Nursing/KarnatakaNursingIndex.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import KarnatakaNursingHero from './KarnatakaNursingHero'
+import WeProvideKarnataka from './WeProvideKarnataka'
 
 
 function KarnatakaNursingIndex() {
   return (
     <div>
         <KarnatakaNursingHero/>
+        <WeProvideKarnataka/>
+        
     </div>
   )
 }
