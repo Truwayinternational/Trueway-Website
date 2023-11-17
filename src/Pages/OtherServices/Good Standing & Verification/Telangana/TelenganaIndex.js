3c2560f56c5a3e9f127490b9042ce80145bc2f8b
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-17 13:00:00 +0530

    Documents for telangana

--- src/Pages/OtherServices/Good Standing & Verification/Telangana/TelenganaIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Telangana/TelenganaIndex.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import TelenganaHero from './TelenganaHero'
 import WeProvideTelengana from './WeProvideTelengana'
+import DocumentsForTelengana from './DocumentsForTelengana'
 
 function TelenganaIndex() {
   return (
     <>
         <TelenganaHero/>
         <WeProvideTelengana/>
-        
+        <DocumentsForTelengana/>
     </>
   )
 }
