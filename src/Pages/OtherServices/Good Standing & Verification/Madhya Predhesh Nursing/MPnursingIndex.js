ffc215e7ffdcf5c7ce4c15deb5188ec532dea313
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-17 11:15:39 +0530

    we provide goodstanding for madhya pradesh nursing council

--- src/Pages/OtherServices/Good Standing & Verification/Madhya Predhesh Nursing/MPnursingIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Madhya Predhesh Nursing/MPnursingIndex.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import MPnursingHero from './MPnursingHero'
+import WeProvideMp from './WeProvideMp'
 
 function MPnursingIndex() {
   return (
     <div>
         <MPnursingHero/>
+        <WeProvideMp/>
     </div>
   )
 }
