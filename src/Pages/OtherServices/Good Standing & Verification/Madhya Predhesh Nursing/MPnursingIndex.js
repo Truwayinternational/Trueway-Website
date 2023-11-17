fb8d4ecc5aaa031d63f81dff851f1a01b6d9a653
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-17 11:24:15 +0530

    Document required for Madhya pradesh nursing council

--- src/Pages/OtherServices/Good Standing & Verification/Madhya Predhesh Nursing/MPnursingIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Madhya Predhesh Nursing/MPnursingIndex.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import MPnursingHero from './MPnursingHero'
 import WeProvideMp from './WeProvideMp'
+import DocumentRequiredForMP from './DocumentRequiredForMP'
 
 function MPnursingIndex() {
   return (
     <div>
         <MPnursingHero/>
         <WeProvideMp/>
+        <DocumentRequiredForMP/>
     </div>
   )
 }
