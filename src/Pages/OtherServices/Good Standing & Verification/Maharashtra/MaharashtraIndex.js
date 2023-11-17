be84479990d163ddbe9cb335ead7f111801e2f9b
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-17 12:07:11 +0530

    Maharashtra Nursing council folder created and added hero, index.js

---
+++ src/Pages/OtherServices/Good Standing & Verification/Maharashtra/MaharashtraIndex.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MaharashtraHero from './MaharashtraHero'
+
+function MaharashtraIndex() {
+  return (
+    <div>
+        <MaharashtraHero/>
+    </div>
+  )
+}
+
+export default MaharashtraIndex
