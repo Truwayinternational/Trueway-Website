8a5901d19e79de68b4e2ec47e4255b5f0a0ef394
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-17 12:53:04 +0530

    we provide goodstanding for telengana

--- src/Pages/OtherServices/Good Standing & Verification/Telangana/TelenganaIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Telangana/TelenganaIndex.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import TelenganaHero from './TelenganaHero'
+import WeProvideTelengana from './WeProvideTelengana'
 
 function TelenganaIndex() {
   return (
     <>
         <TelenganaHero/>
+        <WeProvideTelengana/>
+        
     </>
   )
 }
