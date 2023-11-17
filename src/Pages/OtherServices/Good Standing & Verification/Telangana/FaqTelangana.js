38b273c883306054321ab1b07bf87d08ab60ac06
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-17 13:04:29 +0530

    Faq telengana- completed all in Telangana council

---
+++ src/Pages/OtherServices/Good Standing & Verification/Telangana/FaqTelangana.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import FaqAccordion from '../../FaqAccordion'
+
+function FaqTelangana() {
+
+    const [open, SetOpen] =useState(false)
+
+    const toggle =(index) => {
+        if (open === index) {
+            return SetOpen (null)
+        }
+        SetOpen(index)
+    }
+
+    const AccordionData = [
+        {
+            id:1,
+            question: "How can I renew my nursing certificate in Telangana?",
+            Answer:"Contact Trueway customer care executive; they will guide you on the renewal process."
+        },
+        {
+            id:2,
+            question:"How many days will it take for a good standing certificate from Telangana nursing council?",
+            Answer:"Generally it takes 30 working days."
+        },
+        {
+            id:3,
+            question:"What is the validity period of Telangana Good Standing?",
+            Answer:"6 months."
+        }
+    ]
+
+
+  return (
+    <>
+        <div className='w-full h-full flex flex-col justify-between '>
+            <div className="max-w-[1240px] my-10 m-auto">
+                <div className='flex flex-col justify-center md:items-center w-full px-2 py-4'>
+                    <h2 className='text-4xl lg:text-5xl  tracking-wide font-bold text-center font-HeadingFont py-3'>FAQ</h2>
+                </div>
+                <div className='flex justify-center items-center my-10 '>
+                    <div className=' mx-5 lg:w-[800px] md:w-[500px] '>
+                        {AccordionData.map((data, index)=>{
+                            return <FaqAccordion 
+                                key={index}
+                                open={index === open}
+                                question={data.question}
+                                answer={data.Answer}
+                                toggle={()=> toggle(index)}
+                            />
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default FaqTelangana
--- src/Pages/OtherServices/Good Standing & Verification/Telangana/TelenganaIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Telangana/TelenganaIndex.js
@@ -2,6 +2,10 @@ import React from 'react'
 import TelenganaHero from './TelenganaHero'
 import WeProvideTelengana from './WeProvideTelengana'
 import DocumentsForTelengana from './DocumentsForTelengana'
+import WhyShouldYouChooseUs from '../../../../Components/WhyShouldYouChooseUs'
+import OurClients from '../../OurClients'
+import Form2 from '../../../../Components/Forms/Form2'
+import FaqTelangana from './FaqTelangana'
 
 function TelenganaIndex() {
   return (
@@ -9,6 +13,10 @@ function TelenganaIndex() {
         <TelenganaHero/>
         <WeProvideTelengana/>
         <DocumentsForTelengana/>
+        <WhyShouldYouChooseUs/>
+        <OurClients/>
+        <Form2/>
+        <FaqTelangana/>
     </>
   )
 }
