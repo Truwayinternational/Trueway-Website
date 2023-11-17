e7031368fc81f587d29cc43b98a17d96b4db34f3
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-17 12:17:05 +0530

    Document required for maharashtra nursing

--- src/Pages/OtherServices/Good Standing & Verification/Maharashtra/MaharashtraIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Maharashtra/MaharashtraIndex.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import MaharashtraHero from './MaharashtraHero'
 import WeProvideMaharashtra from './WeProvideMaharashtra'
+import DocumentsForMaharashtra from './DocumentsForMaharashtra'
 
 function MaharashtraIndex() {
   return (
     <div>
         <MaharashtraHero/>
         <WeProvideMaharashtra/>
+        <DocumentsForMaharashtra/>
     </div>
   )
 }
