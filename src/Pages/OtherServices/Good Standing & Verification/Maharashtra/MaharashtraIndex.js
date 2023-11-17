f5398be3cadabecad940312df635a2bb315a7419
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-17 12:11:22 +0530

    we provide goodstanding for Maharashtra

--- src/Pages/OtherServices/Good Standing & Verification/Maharashtra/MaharashtraIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Maharashtra/MaharashtraIndex.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import MaharashtraHero from './MaharashtraHero'
+import WeProvideMaharashtra from './WeProvideMaharashtra'
 
 function MaharashtraIndex() {
   return (
     <div>
         <MaharashtraHero/>
+        <WeProvideMaharashtra/>
     </div>
   )
 }
