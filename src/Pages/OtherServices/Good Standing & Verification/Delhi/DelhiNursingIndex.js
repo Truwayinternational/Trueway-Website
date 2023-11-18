d0a3f76ad07f83160acdb2f1038d567c6da0c940
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-18 11:30:41 +0530

    We provide goodstanding for delhi nursing

--- src/Pages/OtherServices/Good Standing & Verification/Delhi/DelhiNursingIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Delhi/DelhiNursingIndex.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import DelhiNursingHero from './DelhiNursingHero'
+import WeProvideDelhi from './WeProvideDelhi'
 
 function DelhiNursingIndex() {
   return (
     <div>
         <DelhiNursingHero/>
+        <WeProvideDelhi/>
     </div>
   )
 }
