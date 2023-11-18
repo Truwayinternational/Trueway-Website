ce39e1d07008b95abbc63b9bd08b60e070afe1c9
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-18 11:48:39 +0530

    completed - delhi Nursing council, Faq ,testimonial,

--- src/Pages/OtherServices/Good Standing & Verification/Delhi/DelhiNursingIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Delhi/DelhiNursingIndex.js
@@ -2,6 +2,10 @@ import React from 'react'
 import DelhiNursingHero from './DelhiNursingHero'
 import WeProvideDelhi from './WeProvideDelhi'
 import DnocumentsForDelhi from './DnocumentsForDelhi'
+import WhyShouldYouChooseUs from '../../../../Components/WhyShouldYouChooseUs'
+import OurClients from '../../OurClients'
+import Form2 from '../../../../Components/Forms/Form2'
+import FaqDelhiNursing from './FaqDelhiNursing'
 
 function DelhiNursingIndex() {
   return (
@@ -9,6 +13,10 @@ function DelhiNursingIndex() {
         <DelhiNursingHero/>
         <WeProvideDelhi/>
         <DnocumentsForDelhi/>
+        <WhyShouldYouChooseUs/>
+        <OurClients/>
+        <Form2/>
+        <FaqDelhiNursing/>
     </div>
   )
 }
