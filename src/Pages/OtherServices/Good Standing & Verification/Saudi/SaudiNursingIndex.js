77d38c7d7eb38d2b3f17280fc42b55b1b2e5ecef
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-18 13:15:36 +0530

    We provide goodstanding for saudi

--- src/Pages/OtherServices/Good Standing & Verification/Saudi/SaudiNursingIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Saudi/SaudiNursingIndex.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import SaudiNursingHero from './SaudiNursingHero'
+import WeProvideSaudi from './WeProvideSaudi'
 
 function SaudiNursingIndex() {
   return (
     <div>
         <SaudiNursingHero/>
-        
+        <WeProvideSaudi/>
     </div>
   )
 }
