ed4cce6220de8e1704d50aef1a1d980c70b0fbe0
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-18 16:35:30 +0530

    Completed TCMC

--- src/Pages/OtherServices/Good Standing & Verification/TCMC/TCMCindex.js
+++ src/Pages/OtherServices/Good Standing & Verification/TCMC/TCMCindex.js
@@ -3,6 +3,9 @@ import TCMChero from './TCMChero'
 import ContactForGoodstanding from '../ContactForGoodstanding'
 import WeProvideTCMC from './WeProvideTCMC'
 import DocumentsForTCMC from './DocumentsForTCMC'
+import WhyShouldYouChooseUs from '../../../../Components/WhyShouldYouChooseUs'
+import OurClients from '../../OurClients'
+import Form2 from '../../../../Components/Forms/Form2'
 
 function TCMCindex() {
   return (
@@ -11,6 +14,9 @@ function TCMCindex() {
         <ContactForGoodstanding/>
         <WeProvideTCMC/>
         <DocumentsForTCMC/>
+        <WhyShouldYouChooseUs/>
+        <OurClients/>
+        <Form2/>
     </div>
   )
 }
