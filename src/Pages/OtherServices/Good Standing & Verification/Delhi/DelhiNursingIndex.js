a88fcaee019150a8a202519cb3b63bea279b3d77
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-18 11:36:57 +0530

    Documents required for Delhi nursing council

--- src/Pages/OtherServices/Good Standing & Verification/Delhi/DelhiNursingIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Delhi/DelhiNursingIndex.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import DelhiNursingHero from './DelhiNursingHero'
 import WeProvideDelhi from './WeProvideDelhi'
+import DnocumentsForDelhi from './DnocumentsForDelhi'
 
 function DelhiNursingIndex() {
   return (
     <div>
         <DelhiNursingHero/>
         <WeProvideDelhi/>
+        <DnocumentsForDelhi/>
     </div>
   )
 }
