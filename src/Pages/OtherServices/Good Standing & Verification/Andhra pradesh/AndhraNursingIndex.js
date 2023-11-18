7e215f3e34cea97039ea27d95d742866d3ea5ece
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-18 15:22:11 +0530

    Completed Andhra pradesh Nursing council

--- src/Pages/OtherServices/Good Standing & Verification/Andhra pradesh/AndhraNursingIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Andhra pradesh/AndhraNursingIndex.js
@@ -2,6 +2,10 @@ import React from 'react'
 import AndhraNursingHero from './AndhraNursingHero'
 import WeProvideAndhra from './WeProvideAndhra'
 import DocumentsForAndhra from './DocumentsForAndhra'
+import WhyShouldYouChooseUs from '../../../../Components/WhyShouldYouChooseUs'
+import OurClients from '../../OurClients'
+import Form2 from '../../../../Components/Forms/Form2'
+import FaqAndhraNursing from './FaqAndhraNursing'
 
 function AndhraNursingIndex() {
   return (
@@ -9,6 +13,10 @@ function AndhraNursingIndex() {
         <AndhraNursingHero/>
         <WeProvideAndhra/>
         <DocumentsForAndhra/>
+        <WhyShouldYouChooseUs/>
+        <OurClients/>
+        <Form2/>
+        <FaqAndhraNursing/>
     </div>
   )
 }
---
+++ src/Pages/OtherServices/Good Standing & Verification/Andhra pradesh/FaqAndhraNursing.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import FaqAccordion from '../../FaqAccordion'
+
+function FaqAndhraNursing() {
+
+const [open, SetOpen] =useState(false)
+
+    const toggle =(index) => {
+        if (open === index) {
+            return SetOpen (null)
+        }
+        SetOpen(index)
+    }
+
+    const AccordionData = [
+        {
+            id:1,
+            question: "How many days will it take for a good standing certificate from Andhra pradesh nursing council?",
+            Answer:"It typically takes 7 to 10 business days. You can get help from Trueway International to obtain the certificate more quickly and securely."
+        },
+        {
+            id:2,
+            question:"What is the validity period of Andhra Pradesh Good Standing Certificate?",
+            Answer:"Its normal validity duration is one year from the date of issuance."
+        },
+        {
+            id:3,
+            question:"How can I get a good standing certificate from Andhra Pradesh nursing Council?",
+            Answer:"Trueway provides quick and secure assistance with a good standing certificate from the council in Andhra Pradesh."
+        },
+        {
+            id:4,
+            question:"What is the name of Andhra Pradesh nursing Council?",
+            Answer:"The Andhra Pradesh Nursing Council, also known as the APNMC or A.P. Nurses and Midwives Council, works to uphold the highest standards for nursing and midwifery education in order to safeguard the public's health and safety."
+        }
+    ]
+
+  return (
+    <>
+        <div className='w-full h-full flex flex-col justify-between '>
+            <div className="max-w-[1240px] my-10 m-auto">
+                <div className='flex flex-col justify-center md:items-center w-full px-2 py-4'>
+                    <h2 className='text-4xl lg:text-5xl  tracking-wide font-bold text-center font-HeadingFont py-3'>FAQ</h2>
+                </div>
+                <div className='flex justify-center items-center my-10 '>
+                    <div className=' mx-5 lg:w-[800px] md:w-[500px] '>
+                        {AccordionData.map((data, index)=>{
+                            return <FaqAccordion 
+                                key={index}
+                                open={index === open}
+                                question={data.question}
+                                answer={data.Answer}
+                                toggle={()=> toggle(index)}
+                            />
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default FaqAndhraNursing
