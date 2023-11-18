d31af3c061f537f91553f8e1a01c172efadc07c9
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-18 13:32:10 +0530

    Saudi Nursing council - Completed

--- src/Pages/OtherServices/Good Standing & Verification/Saudi/SaudiNursingIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Saudi/SaudiNursingIndex.js
@@ -2,6 +2,10 @@ import React from 'react'
 import SaudiNursingHero from './SaudiNursingHero'
 import WeProvideSaudi from './WeProvideSaudi'
 import DocumentsForSaudi from './DocumentsForSaudi'
+import WhyShouldYouChooseUs from '../../../../Components/WhyShouldYouChooseUs'
+import OurClients from '../../OurClients'
+import Form2 from '../../../../Components/Forms/Form2'
+import FaqSaudiNursing from './FaqSaudiNursing'
 
 function SaudiNursingIndex() {
   return (
@@ -9,6 +13,10 @@ function SaudiNursingIndex() {
         <SaudiNursingHero/>
         <WeProvideSaudi/>
         <DocumentsForSaudi/>
+        <WhyShouldYouChooseUs/>
+        <OurClients/>
+        <Form2/>
+        <FaqSaudiNursing/>
     </div>
   )
 }
