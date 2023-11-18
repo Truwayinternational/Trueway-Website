aea0a91d9a0b94d9677a247a3ee583102463a4ef
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-18 16:26:25 +0530

    TCMC WE PROVID goodstanding

--- src/Pages/OtherServices/Good Standing & Verification/TCMC/TCMCindex.js
+++ src/Pages/OtherServices/Good Standing & Verification/TCMC/TCMCindex.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import TCMChero from './TCMChero'
 import ContactForGoodstanding from '../ContactForGoodstanding'
+import WeProvideTCMC from './WeProvideTCMC'
 
 function TCMCindex() {
   return (
     <div>
         <TCMChero/>
         <ContactForGoodstanding/>
+        <WeProvideTCMC/>
     </div>
   )
 }
