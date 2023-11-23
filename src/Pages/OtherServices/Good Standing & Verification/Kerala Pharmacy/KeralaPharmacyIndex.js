d094942357fe3fe84b1ad29e4c410d3cdcf1b7da
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-23 12:02:04 +0530

    kerala pharmacy created and added hero, index,js

---
+++ src/Pages/OtherServices/Good Standing & Verification/Kerala Pharmacy/KeralaPharmacyIndex.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import KeralaPharmacyHero from './KeralaPharmacyHero'
+
+function KeralaPharmacyIndex() {
+  return (
+    <div>
+        <KeralaPharmacyHero/>
+    </div>
+  )
+}
+
+export default KeralaPharmacyIndex
