18563a0a7ac60d5102d1910b2087e2035ac76ea5
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-23 11:46:28 +0530

    completed kerala paramedical council

--- src/Pages/OtherServices/Good Standing & Verification/Kerala Paramedical/KeralaParamedicalIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Kerala Paramedical/KeralaParamedicalIndex.js
@@ -3,6 +3,9 @@ import KeralaParamedicalHero from './KeralaParamedicalHero'
 import ContactForGoodstanding from '../ContactForGoodstanding'
 import WeProvideParamedical from './WeProvideParamedical'
 import DocumentsForParamedical from './DocumentsForParamedical'
+import WhyShouldYouChooseUs from '../../../../Components/WhyShouldYouChooseUs'
+import OurClients from '../../OurClients'
+import Form2 from '../../../../Components/Forms/Form2'
 
 function KeralaParamedicalIndex() {
   return (
@@ -11,6 +14,9 @@ function KeralaParamedicalIndex() {
         <ContactForGoodstanding/>
         <WeProvideParamedical/>
         <DocumentsForParamedical/>
+        <WhyShouldYouChooseUs/>
+        <OurClients/>
+        <Form2/>
     </>
   )
 }
