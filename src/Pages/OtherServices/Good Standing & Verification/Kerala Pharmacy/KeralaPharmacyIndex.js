3fbdcc049a16206cdd5091a4cb7f76c0263942d7
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-23 12:19:03 +0530

    completed kerala pharmacy council

--- src/Pages/OtherServices/Good Standing & Verification/Kerala Pharmacy/KeralaPharmacyIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Kerala Pharmacy/KeralaPharmacyIndex.js
@@ -3,6 +3,9 @@ import KeralaPharmacyHero from './KeralaPharmacyHero'
 import ContactForGoodstanding from '../ContactForGoodstanding'
 import WeProvidePharmacy from './WeProvidePharmacy'
 import DocumentsForPharmacy from './DocumentsForPharmacy'
+import WhyShouldYouChooseUs from '../../../../Components/WhyShouldYouChooseUs'
+import OurClients from '../../OurClients'
+import Form2 from '../../../../Components/Forms/Form2'
 
 function KeralaPharmacyIndex() {
   return (
@@ -11,6 +14,9 @@ function KeralaPharmacyIndex() {
         <ContactForGoodstanding/>
         <WeProvidePharmacy/>
         <DocumentsForPharmacy/>
+        <WhyShouldYouChooseUs/>
+        <OurClients/>
+        <Form2/>
     </div>
   )
 }
