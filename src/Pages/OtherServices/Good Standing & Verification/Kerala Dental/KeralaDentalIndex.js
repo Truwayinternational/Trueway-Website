57cd1f6bfce15959d070896e7c809e28800844f0
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-23 10:50:19 +0530

    We provide gcc countries

--- src/Pages/OtherServices/Good Standing & Verification/Kerala Dental/KeralaDentalIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Kerala Dental/KeralaDentalIndex.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import KeralaDentalHero from './KeralaDentalHero'
 import ContactForGoodstanding from '../ContactForGoodstanding'
+import WeProvideDental from './WeProvideDental'
 
 function KeralaDentalIndex() {
   return (
     <div>
         <KeralaDentalHero/>
         <ContactForGoodstanding/>
-        
+        <WeProvideDental/>
     </div>
   )
 }
