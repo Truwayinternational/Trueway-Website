682bf16d555672c9ed6e8739511a366f447ef104
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-23 12:09:34 +0530

    We provide contreis for pharmacy council

--- src/Pages/OtherServices/Good Standing & Verification/ContactForGoodstanding.js
+++ src/Pages/OtherServices/Good Standing & Verification/ContactForGoodstanding.js
@@ -10,7 +10,7 @@ function ContactForGoodstanding() {
         <div className='max-w-3xl mx-auto place-content-center my-10'>
             <div className='grid justify-center bg-zinc-100 rounded-3xl mx-5 py-3 px-5'>
                 <h6 className='md:text-2xl font-bold font-HeadingFont text-center text-xl  my-5'> 
-                To pursue your good standing certificate; <br/> Reach out to us
+                If you are facing trouble in pursuing your good standing certificate: Reach out to us
                 </h6>
                 <div className='flex items-center my-3 mx-auto'>
                     <div className='bg-white rounded-full mx-5 p-3 hover:shadow-xl'>
--- src/Pages/OtherServices/Good Standing & Verification/Kerala Pharmacy/KeralaPharmacyIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Kerala Pharmacy/KeralaPharmacyIndex.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import KeralaPharmacyHero from './KeralaPharmacyHero'
+import ContactForGoodstanding from '../ContactForGoodstanding'
+import WeProvidePharmacy from './WeProvidePharmacy'
 
 function KeralaPharmacyIndex() {
   return (
     <div>
         <KeralaPharmacyHero/>
+        <ContactForGoodstanding/>
+        <WeProvidePharmacy/>
     </div>
   )
 }
