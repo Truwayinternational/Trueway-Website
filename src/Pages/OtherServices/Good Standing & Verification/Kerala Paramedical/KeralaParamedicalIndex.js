9205b5eb1784fff6a50c68257d357800f317382c
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-23 11:37:55 +0530

    We provide paramedical for these countries

--- src/Pages/OtherServices/Good Standing & Verification/Kerala Paramedical/KeralaParamedicalIndex.js
+++ src/Pages/OtherServices/Good Standing & Verification/Kerala Paramedical/KeralaParamedicalIndex.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import KeralaParamedicalHero from './KeralaParamedicalHero'
 import ContactForGoodstanding from '../ContactForGoodstanding'
+import WeProvideParamedical from './WeProvideParamedical'
 
 function KeralaParamedicalIndex() {
   return (
     <>
         <KeralaParamedicalHero/>
         <ContactForGoodstanding/>
+        <WeProvideParamedical/>
     </>
   )
 }
