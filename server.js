8155be61e1d825651f56dcbdd04b198010003fea
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-24 16:03:57 +0530

    get in touch form submission completed

--- server.js
+++ server.js
@@ -27,7 +27,35 @@ contactEmail.verify((error) => {
   }
 });
 
+// form submission from contact us page
 router.post("/contact", (req, res) => {
+  const name = req.body.name;
+  const number = req.body.number;
+  const email = req.body.email;
+  const message = req.body.message;
+  const mail = {
+    from: email,
+    to: "tvm@truewayinternational.com",
+    subject: "Trueway Website Contact Form Submission ",
+    html: `
+            <p>Name: ${name}</p>
+            <p>Number: ${number}</p>
+            <p>Email: ${email}</p>
+            <p>Message: ${message}</p>
+          `,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json({ status: "ERROR" });
+    } else {
+      res.json({ status: "Form submitted successfully" });
+    }
+  });
+});
+
+
+// form submission from main home page (Form-Get in touch)
+router.post("/", (req, res) => {
   const name = req.body.name;
   const number = req.body.number;
   const email = req.body.email;
@@ -51,3 +79,5 @@ router.post("/contact", (req, res) => {
     }
   });
 });
+
+
