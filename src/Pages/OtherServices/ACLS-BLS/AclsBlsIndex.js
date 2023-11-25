d4f11e42fbb7b8ab2c0d4fae284cebdcf2f8f2fd
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-25 12:25:56 +0530

    ACLS section

--- src/Pages/OtherServices/ACLS-BLS/AclsBlsIndex.js
+++ src/Pages/OtherServices/ACLS-BLS/AclsBlsIndex.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AclsBlsHero from './AclsBlsHero'
+import AdvancedCardio from './AdvancedCardio'
 
 function AclsBlsIndex() {
   return (
     <div>
         <AclsBlsHero/>
+        <AdvancedCardio/>
     </div>
   )
 }
